74969d0c863c718392986c64e2e756a369cce287
Danilo Vitoriano <vitoriano08@gmail.com>
2022-05-07 13:56:43 -0300

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Danilo Vitoriano.
+      <time>{YEAR}</time> © Dan Vitoriano.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
